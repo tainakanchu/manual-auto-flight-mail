ad582b927701b4298a8193998ff8fe4a88e1df3f
tainakanchu <tainakanchu815@gmail.com>
2024-01-06 01:59:22 +0900

    Refactor useDatePicker hook to handle date and time changes

--- app/_hooks/useDatePicker.tsx
+++ app/_hooks/useDatePicker.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState } from "react";
+import { format, set } from "date-fns";
+import { useCallback, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -7,6 +8,7 @@ import "react-datepicker/dist/react-datepicker.css";
  * 日付を選択するためのカスタムフック
  *
  * @param initialDate 初期値
+ * @param onChange 日付が変更されたときに呼ばれる関数
  * @returns
  * - date: 選択された日付
  * - setDate: 日付を設定する関数
@@ -15,28 +17,59 @@ import "react-datepicker/dist/react-datepicker.css";
 export const useDatePicker = ({
   initialDate = new Date(),
   onChange = () => {},
-}: {
-  initialDate?: Date;
-  onChange?: (date: Date) => void;
-}) => {
+}: Partial<{
+  initialDate: Date;
+  onChange: (date: Date) => void;
+}> = {}) => {
   const [date, setDate] = useState(initialDate);
 
-  const component = () => (
-    <DatePicker
-      className="border-2 border-gray-300 rounded-lg p-2 w-96 text-gray-900"
-      dateFormat="yyyy/MM/dd HH:mm"
-      locale="ja"
-      selected={date}
-      onChange={(d) => {
-        if (d) {
-          setDate(d);
-          onChange(d);
-        }
-      }}
-      showTimeSelect
-      timeIntervals={5}
-      timeFormat="HH:mm"
-    />
+  const dateRef = useRef<Date>(new Date());
+
+  const handleSetDate = useCallback(
+    (d: Date) => {
+      setDate(d);
+      onChange(d);
+    },
+    [onChange]
+  );
+
+  const component = useCallback(
+    () => (
+      <DatePicker
+        className="border-2 border-gray-300 rounded-lg p-2 w-96 text-gray-900"
+        dateFormat="yyyy/MM/dd HH:mm"
+        selected={date}
+        onChange={(d) => {
+          if (!d) return;
+
+          // HACK: onChangeは、日付、時間を設定した時どちらも呼ばれてくる。
+          // 時間を指定した時だけsetDateをしたい（=ダイアログを閉じるようにしたい）ため、日付が変わったかどうかで処理を変える
+          const isDateChanged =
+            format(d, "yyyy/MM/dd") !== format(date, "yyyy/MM/dd");
+
+          if (isDateChanged) {
+            // 時間が変更されていない場合は、refに日付を保存するだけ（ここでsetDateするとダイアログが閉じてしまう）
+            dateRef.current = d;
+          } else {
+            // 時間が設定されたときは、refに保存した日付に時間をセットして目的の日付・時間を作ってセットする
+            const newDate = set(dateRef.current, {
+              hours: d.getHours(),
+              minutes: d.getMinutes(),
+            });
+            handleSetDate(newDate);
+          }
+        }}
+        onCalendarClose={() => {
+          // 日付だけが変更された場合はここに来る。その場合はrefに保存した日付をセットする
+          handleSetDate(dateRef.current);
+        }}
+        showTimeSelect
+        timeIntervals={5}
+        timeFormat="HH:mm"
+        showTwoColumnMonthYearPicker
+      />
+    ),
+    [date, handleSetDate]
   );
 
   return {
